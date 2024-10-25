1a5dc5fb39db45d5bba457d79518684665e2dacc
dell <patilayush8788@gmail.com>
2024-10-25 11:05:15 +0530

    Api response file utils updated

---
+++ src/utils/Apiresponse.js
@@ -0,0 +1,12 @@
+class   Apriresponse   {
+    constructor(
+        statuscode,
+        message="success",
+        data
+    ){
+        this.statuscode=statuscode,
+        this.message=message,
+        this.data=data,
+        this.success=statuscode<400
+    }
+}
