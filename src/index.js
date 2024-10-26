7c341dfa8ebc1d24c06fb583be97986d8159a563
dell <patilayush8788@gmail.com>
2024-10-26 16:16:46 +0530

    all setup confgured

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import  connectDB   from "./db/db.js"
 import  dotenv  from    'dotenv'
+import  {app} from    './app.js'
 
 dotenv.config({
     path:'./env'
@@ -11,7 +12,7 @@ connectDB()
         console.log("Error",error);
         throw   error
     })
-    app.listen(process.env.PORT ||8000,()=>{
+    app.listen(process.env.PORT||8000,()=>{
         console.log(` server is listning    on:${process.env.PORT}`);
         
     })
